9e380f0a86a31fc03ec8cbd2dcb616927a9cebb4
elsie <elsiechen915@gmail.com>
2023-06-18 22:09:03 -0400

    Build a form for location input

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const weatherBtn = document.querySelector('#weatherBtn');
+weatherBtn.addEventListener('click', () => {
+    let locationInput = document.querySelector('#locationInput');
+    let errorField = document.querySelector('.error');
+
+    // Form validation
+    errorField.textContent = '';
+    if (locationInput.validity.valueMissing) {
+        errorField.textContent = 'You need to enter something to search for.';
+        return;
+    }
+    // Fetch data
+    fetchWeather(locationInput.value);
+});
+
 async function fetchWeather(location) {
     try {
         const url = `https://api.weatherapi.com/v1/current.json?key=0f44ee531494426cb6b20040231306&q=${location}`;
@@ -7,6 +22,7 @@ async function fetchWeather(location) {
             throw Error(getData.error.message);
         };
         console.log(getData);
+        console.log(typeof getData);
     } catch(err) {
         console.log(err);
     }
